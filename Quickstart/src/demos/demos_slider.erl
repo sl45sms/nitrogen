63bb05b684350934036188d02c1b5b39b0e5745b
Jan-Felix Wittmann <jfwittmann@gmx.de>
2010-07-09 10:43:10 +0200

    update slider demo

--- Quickstart/src/demos/demos_slider.erl
+++ Quickstart/src/demos/demos_slider.erl
@@ -19,7 +19,7 @@ left() ->
 
 right() ->
     %% wire slider to slide event. 
-    wf:wire(slider_percent, #event {type=slide, actions=#script { script=" 
+    wf:wire(slider_percent, #event {type=slide, actions=#script { script="
         var sliderValue = objs('slider_percent').slider('value');
         objs('textbox_percent').val(sliderValue);
       "}
@@ -29,16 +29,23 @@ right() ->
         #p{},
         #label { text="Percent:"},
         #textbox{ id=textbox_percent },
-        #slider { id=slider_percent, tag=test_slider },
-        #button { text="Post", postback=post }
+        #slider { id=slider_percent, tag=test_slider, animate=true },
+        #button { text="Post", postback=post },
+        #button { text="Set Value to 50", postback=set_value }
     ].
 
 slider_change_event(ChangeValue, _Tag) ->
     Message = wf:f("Change Event Value: ~p~n", [ChangeValue]),
     wf:flash(Message),
     ok.
+ 
 event(post) ->
     Message = wf:f("POST Value: ~p~n", [wf:q(slider_percent)]),
     wf:flash(Message),
     ok;
+event(set_value) ->
+    Value = 50,
+    wf:wire( #script { 
+      script= wf:f("objs('slider_percent').slider('value',~p)",[Value])
+    });
 event(_) -> ok.
